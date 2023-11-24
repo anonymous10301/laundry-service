4629bf027661639224189953e3e23a501e4ca822
Iamneo <techadmin@iamneo.ai>
2023-11-24 09:59:47 +0000

    Backup Commit

---
+++ angularapp/src/models/user-schedule.model.ts
@@ -0,0 +1,15 @@
+import { ɵangular_packages_common_http_http_b } from "@angular/common/http"
+
+export class UserSchedule {
+    Id:number
+    FullName:string
+    MobileNumber:string
+    Email:string
+    Address:string
+    Area:string
+    Pincode:string
+    PickupDay:string
+    PickupTimeSlot:string
+    PackageId:number
+    Package?:ɵangular_packages_common_http_http_b
+}
